365a03ddee95e0d245011295bcfca7020b29c235
Antal István Miklós <istvan.m.antal@gmail.com>
2013-11-09 10:51:47 +0000

    Basic version with basic test ported from PHP

--- src/FuzzyTime.js
+++ src/FuzzyTime.js
@@ -1,3 +1,55 @@
-function FuzzyTime() {
+var FuzzyTime = {
+    format: function ($diff) {
+        var $breakpoints = {
+                1: ['second', 'seconds'],
+                60: ['minute', 'minutes'],
+                3600: ['hour', 'hours'],
+                86400: ['day', 'days']
+            },
+            $keys = [],
+            i,
+            $prev_value = 0,
+            $text = '',
+            $value,
+            $unit,
+            $units;
     
-}
+        if (typeof $diff !== 'number' || isNaN($diff)) {
+            throw new Error('Only numbers are accepted');
+        }
+        
+        for (i in $breakpoints) {
+            if ($breakpoints.hasOwnProperty(i)) {
+                $keys.push(i);
+            }
+        }
+        
+        do {
+            $value = $keys.pop();
+            $unit = $breakpoints[$value];
+            
+            $units = $diff;
+            if ($prev_value) {
+                $units %= $prev_value;
+            }
+            
+            $units /= $value;
+            $units = Math.floor(Math.abs($units));
+            
+            if ($units) {
+                $text += $units + ' ' + $unit[($units > 1) ? 1 : 0] + ' ';
+            }
+            
+            $prev_value = $value;
+            
+        } while ($keys.length);
+        
+        if ($diff < 0) {
+            $text += 'before';
+        } else {
+            $text += 'ago';
+        }
+        
+        return $text;
+    }
+};
--- tests/FuzzyTimeSpec.js
+++ tests/FuzzyTimeSpec.js
@@ -1,6 +1,16 @@
-describe("Dummy", function() {
+describe("FuzzyTime", function() {
 
-    it("Dummy", function() {
-        expect(true).toBe(true);
+    it("Should throw exceptions for invalid inputs", function() {
+        expect(function () { FuzzyTime.format(''); }).toThrow();
+        expect(function () { FuzzyTime.format({}); }).toThrow();
+        expect(function () { FuzzyTime.format(false); }).toThrow();
+        expect(function () { FuzzyTime.format(null); }).toThrow();
+        expect(function () { FuzzyTime.format(NaN); }).toThrow();
+        expect(function () { FuzzyTime.format([]); }).toThrow();
+    });
+    
+    it("Should pluralize properly", function() {
+        expect(FuzzyTime.format(1)).toBe('1 second ago');
+        expect(FuzzyTime.format(2)).toBe('2 seconds ago');
     });
 });
