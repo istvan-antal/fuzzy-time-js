94fc26ab12cfe605789bf4355b46a7976c22e82a
Antal István Miklós <istvan.m.antal@gmail.com>
2013-11-09 11:04:48 +0000

    Added a more complex test

--- tests/FuzzyTimeSpec.js
+++ tests/FuzzyTimeSpec.js
@@ -1,4 +1,8 @@
 describe("FuzzyTime", function() {
+    var minute = 60,
+        hour = 3600,
+        day = 86400,
+        week = 604800;
 
     it("should throw exceptions for invalid inputs", function() {
         expect(function () { FuzzyTime.format(''); }).toThrow();
@@ -19,12 +23,17 @@ describe("FuzzyTime", function() {
     });
     
     it("should work properly when it needs to use combined metrics", function() {
-        expect(FuzzyTime.format(60 + 1)).toBe('1 minute 1 second ago');
-        expect(FuzzyTime.format(120 + 1)).toBe('2 minutes 1 second ago');
+        expect(FuzzyTime.format(minute + 1)).toBe('1 minute 1 second ago');
+        expect(FuzzyTime.format(2 * minute + 1)).toBe('2 minutes 1 second ago');
     });
     
     it("should handle weeks", function() {
-        expect(FuzzyTime.format(604800 + 60 + 1)).toBe('1 week 1 minute 1 second ago');
-        expect(FuzzyTime.format(604800 + 120 + 1)).toBe('1 week 2 minutes 1 second ago');
+        expect(FuzzyTime.format(week + minute + 1)).toBe('1 week 1 minute 1 second ago');
+        expect(FuzzyTime.format(week + 2 * minute + 1)).toBe('1 week 2 minutes 1 second ago');
+    });
+    
+    it("should handle cases with 5 metrics", function() {
+        expect(FuzzyTime.format(week * 2 + day + hour + minute + 1)).toBe('2 weeks 1 day 1 hour 1 minute 1 second ago');
+        expect(FuzzyTime.format(week * 2 + 3 * day + 8 * hour + 2 * minute + 1)).toBe('2 weeks 3 days 8 hours 2 minutes 1 second ago');
     });
 });
